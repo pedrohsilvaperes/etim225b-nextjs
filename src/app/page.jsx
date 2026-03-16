fda13e4443d24cdf9bfcc2235316bd2596d7e81f
pedrohsilvaperes <pedroh.silvaperes@gmail.com>
2026-03-16 15:06:26 -0300

    atualizado ate aula sem 16-03

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+import styles from "./page.module.css"; 
+
+function Home() {
+  return (
+    <div className={styles.containerHome} > 
+      <div className={styles.containerListas}>
+        <h1>Exemplos</h1>
+        <Link href="/exemplos/ex-01">Exemplo 1</Link>
+        <Link href="/exemplos/ex-02">Exemplo 2</Link>        
+      </div>
+      <div className={styles.containerListas}>
+        <h1>Atividades</h1>
+        <Link href="/atividades/atv-01">Atividade 1</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
